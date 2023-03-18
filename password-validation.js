fc17e75bc6cd64c20a379298f1ef523a089d2409
Akhilesh m t <akhileshthykkat843@gmail.com>
2023-03-18 23:16:56 +0530

    Initial commit

---
+++ password-validation.js
@@ -0,0 +1,17 @@
+
+const usernameInput = document.getElementById('username');
+const passwordInput = document.getElementById('password');
+const loginButton = document.getElementById('login-button');
+
+
+loginButton.addEventListener('click', () => {
+
+  if (usernameInput.value === 'akhilesh' && passwordInput.value === 'akhilesh') {
+    //alert('Login successful!');
+
+    window.location.href = 'dashboard.html';
+  } else {
+    alert('Username or password is incorrect.');
+    window.location.href = 'index.html';
+  }
+});
